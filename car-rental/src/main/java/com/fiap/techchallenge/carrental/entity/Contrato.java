2a1b0393518e0f8c4ff154c83766f2ae815f3aa8
LeonardoArantes <leonardo-arantes@live.com>
2023-10-25 00:03:43 -0300

    tokenGenerator

--- car-rental/src/main/java/com/fiap/techchallenge/carrental/entity/Contrato.java
+++ car-rental/src/main/java/com/fiap/techchallenge/carrental/entity/Contrato.java
@@ -1,7 +1,5 @@
 package com.fiap.techchallenge.carrental.entity;
 
-import lombok.Setter;
-
 import java.time.format.DateTimeFormatter;
 
 import jakarta.persistence.Entity;
@@ -13,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @Setter
@@ -39,17 +38,15 @@ public class Contrato {
    public void geradorToken(Reserva reserva){
       final String nomeCliente = reserva.getCliente().getUsuario().getLogin().toUpperCase(); 
       final String veiculo = reserva.getVeiculo().getModelo().toUpperCase();
+
       DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM");
 
       final String dataIni = reserva.getDataInicio().format(formato);
       final String dataFim = reserva.getDataFim().format(formato);
       
-      final String code = ""; 
+      final String div = "#"; 
       //NOME#VEICULO#00/00#00/00
-      token.setToken(code);
+      setToken(nomeCliente+div+veiculo+div+dataIni+div+dataFim);
    }
-
-
-
 
 }
